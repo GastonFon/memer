15115cfa0e2e1ed557f60eea48e2b6efc07dfac8
Lucas Hernán Tarche <lucastarche@gmail.com>
2020-07-30 13:05:03 -0300

    Prueba cambio de página

--- memer.py
+++ memer.py
@@ -1,6 +1,7 @@
 import discord
 import json
 import os 
+import re
 
 from MemeManager import MemeManager
 
@@ -33,7 +34,9 @@ async def on_message(message):
             except ValueError:
                 page = 1
             finally:
-                await message.channel.send(help(page))
+                helpMsg = await message.channel.send(help(page))
+                await message.channel.add_reaction(helpMsg, ":arrow_left:")
+                await message.channel.add_reaction(helpMsg, ":arrow_right:")
                 return
         
         memeManager = MemeManager(msg)
@@ -48,6 +51,27 @@ async def on_message(message):
             await message.delete()
         except discord.errors.Forbidden:
             print("Tried to delete message")
+
+@client.event
+async def on_reaction_add(reaction, user):
+    message = reaction.message
+    msg = str(message)
+    if message.author != client.user:
+        return
+    if str(reaction) != ":arrow_left:" and str(reaction.emoji) != ":arrow_right:":
+        return
+    if not msg.startswith("```asciidoc"):
+        return
+    pageRegex = re.compile(r"\d+\]")
+    page = pageRegex.search(msg).group(0)[:-1]
+    page = int(page)
+    if str(reaction) == ":arrow_left:":
+        page = max(page - 1, 1)
+    elif str(reaction) == ":arrow_right:":
+        page = page + 1
+    message.edit(content=help(page))
+
+    
 
 def help(page):
     with open("metadata.json", "r") as metadata:
